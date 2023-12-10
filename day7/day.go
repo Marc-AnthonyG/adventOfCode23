ffb935ac334cd6af734c7b445a6d4e59c2585b14
Marc-Anthony Girard <marcanthony.girard.pro@gmail.com>
2023-12-10 17:20:01 -0500

    part1

--- day7/day.go
+++ day7/day.go
@@ -14,20 +14,23 @@ func Run(input string) {
 
 type Hand struct {
 	card string
-	bid int
+	bid  int
 }
 
-func parseInput(input string) []Hand{
+func parseInput(input string) []Hand {
 	lines := strings.Split(input, "\n")
 	hands := make([]Hand, 0)
-	
-	for _, line := range lines{
+
+	for _, line := range lines {
+		if line == "" {
+			continue
+		}
 		handStr := strings.Fields(line)
 		bid, _ := strconv.Atoi(handStr[1])
 
 		hands = append(hands, Hand{handStr[0], bid})
 	}
-	
+
 	return hands
 }
 
@@ -37,12 +40,120 @@ func Part2(input string) int {
 
 func Part1(input string) int {
 	hands := parseInput(input)
+	result := make([][]Hand, 7)
 
-	for _, hand := range hands{
-		fmt.Println(hand)
+	for _, hand := range hands {
+		test := getCardFormated(hand.card)
+		isPair := false
+		isThree := false
+		isFour := false
+		isFive := false
+		hasTwoPair := false
+
+		for _, value := range test {
+			if value == 2 {
+				hasTwoPair = isPair
+				isPair = true
+			} else if value == 3 {
+				isThree = true
+			} else if value == 4 {
+				isFour = true
+			} else if value == 5 {
+				isFive = true
+			}
+		}
+
+		if isFive {
+			result[0] = appendInOrder(result[0], hand)
+		} else if isFour {
+			result[1] = appendInOrder(result[1], hand)
+		} else if isThree && isPair {
+			result[2] = appendInOrder(result[2], hand)
+		} else if isThree {
+			result[3] = appendInOrder(result[3], hand)
+		} else if isPair && hasTwoPair {
+			result[4] = appendInOrder(result[4], hand)
+		}else if isPair {
+			result[5] = appendInOrder(result[5], hand)
+		} else {
+			result[6] = appendInOrder(result[6], hand)
+		}
+	}
+
+	globalResult := 0
+	conteur := len(hands)
+	fmt.Println(conteur)
+
+	for _, value := range result {
+		fmt.Println(value)
+		for _, hand := range value {
+			globalResult += hand.bid * conteur
+			conteur--
+		}
+	}
+
+	return globalResult
+}
+
+func appendInOrder(hands []Hand, hand Hand) []Hand {
+	min := 0
+	max := len(hands)
+
+	for min < max {
+		mid := (min + max) / 2
+
+		for i := 0; i < len(hand.card); i++ {
+			if hands[mid].card[i] == hand.card[i] {
+				continue
+			} else if compare(rune(hand.card[i]), rune(hands[mid].card[i])) {
+				max = mid
+				break
+			} else {
+				min = mid + 1
+				break
+			}
+		}
+	}
+	
+	return insert(hands, min, hand)
+}
+
+func insert(a []Hand, index int, value Hand) []Hand {
+    if len(a) == index { // nil or empty slice or after last element
+        return append(a, value)
+    }
+    a = append(a[:index+1], a[index:]...) // index < len(a)
+    a[index] = value
+    return a
+}
+
+func compare(r1 rune, r2 rune) bool {
+	if r1 == 'A' {
+		return true
+	} else if r1 == 'K' {
+		return r2 != 'A'
+	} else if r1 == 'Q' {
+		return r2 != 'A' && r2 != 'K'
+	} else if r1 == 'J' {
+		return r2 != 'A' && r2 != 'K' && r2 != 'Q'
+	} else if r1 == 'T' {
+		return r2 != 'A' && r2 != 'K' && r2 != 'Q' && r2 != 'J'
 	}
 	
 
-	return -1
+	return r1 > r2
 }
 
+func getCardFormated(cards string) map[rune]int {
+	result := map[rune]int{}
+
+	for _, card := range cards {
+		if _, ok := result[card]; ok {
+			result[card]++
+		} else {
+			result[card] = 1
+		}
+	}
+
+	return result
+}
--- day7/day_test.go
+++ day7/day_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 )
 
-var input = "32T3K 765\nT55J5 684\nKK677 28\nKTJJT 220\nQQQJA 483"
+var input = "32T3K 765\nAJ6J6 1\nQQ66A 1\nT55J5 684\nKK677 28\nKTJJT 220\nQQQJA 483"
 
 func Test_Day7Part1(t *testing.T) {
 	// given
@@ -12,7 +12,7 @@ func Test_Day7Part1(t *testing.T) {
 	actual := Part1(input)
 
 	if actual != expected {
-		t.Errorf("Part one was supposed to returned %d, expected %d", actual, expected)
+		t.Errorf("Part one was supposed to returned %d, got %d", expected, actual)
 	}
 }
 
